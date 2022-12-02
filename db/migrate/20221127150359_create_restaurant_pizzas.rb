eef07ac2b5735a0bb543d6d678c2f029d91896b0
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-12-02 23:38:25 +0300

    create associations for pizzas and restaurants

--- db/migrate/20221127150359_create_restaurant_pizzas.rb
+++ db/migrate/20221127150359_create_restaurant_pizzas.rb
@@ -1,7 +1,8 @@
 class CreateRestaurantPizzas < ActiveRecord::Migration[7.0]
   def change
     create_table :restaurant_pizzas do |t|
-
+      t.belongs_to :pizza
+      t.belongs_to :restaurant
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_27_150334) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_27_150359) do
   create_table "pizzas", force: :cascade do |t|
     t.string "name"
     t.string "ingredients"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "restaurant_pizzas", force: :cascade do |t|
+    t.integer "pizza_id"
+    t.integer "restaurant_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["pizza_id"], name: "index_restaurant_pizzas_on_pizza_id"
+    t.index ["restaurant_id"], name: "index_restaurant_pizzas_on_restaurant_id"
   end
 
   create_table "restaurants", force: :cascade do |t|
