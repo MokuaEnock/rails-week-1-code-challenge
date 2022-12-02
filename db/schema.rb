047b336acdab1a33ef94134370a46d6c59524c55
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-12-02 23:33:39 +0300

    revert changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_27_150359) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_27_150334) do
   create_table "pizzas", force: :cascade do |t|
     t.string "name"
     t.string "ingredients"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "restaurant_pizzas", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
