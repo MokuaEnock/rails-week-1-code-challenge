dfc37af0340cc11e65fc0fa3a12fc838cc130c0a
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-12-03 01:03:50 +0300

    display index and show for pizzas

--- app/controllers/pizzas_controller.rb
+++ app/controllers/pizzas_controller.rb
@@ -1,2 +1,11 @@
 class PizzasController < ApplicationController
+  def index
+    pizzas = Pizza.all
+    render json: pizzas
+  end
+
+  def show
+    pizza = Pizza.find(params[:id])
+    render json: pizza
+  end
 end
