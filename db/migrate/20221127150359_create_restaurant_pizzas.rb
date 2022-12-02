1b055dd8942e7ab793d137bdb0b24264feb79ffa
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-12-03 00:53:31 +0300

    create routes

--- db/migrate/20221127150359_create_restaurant_pizzas.rb
+++ db/migrate/20221127150359_create_restaurant_pizzas.rb
@@ -3,6 +3,8 @@ class CreateRestaurantPizzas < ActiveRecord::Migration[7.0]
     create_table :restaurant_pizzas do |t|
       t.belongs_to :pizza
       t.belongs_to :restaurant
+      # t.integer :pizza_id
+      # t.integer :restaurant_id
       t.timestamps
     end
   end
