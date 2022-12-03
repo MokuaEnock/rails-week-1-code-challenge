2ab91cf85643a32e38dca795fd90f825014b658a
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-12-03 06:14:53 +0300

    methods for Restaurants

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,19 @@
 class RestaurantsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+  def index
+    restaurants = Restaurant.all
+    render json: restaurants
+  end
+
+  def show
+    restaurant = Restaurant.find_by(params[:id])
+    render json: restaurant
+  end
+
+  private
+
+  def render_not_found_response
+    render json: { error: "Author not found" }, status: :not_found
+  end
 end
