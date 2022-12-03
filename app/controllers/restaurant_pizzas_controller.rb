7dcb81e1436689c7b699159d4cf81dfb0920d000
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-12-03 07:03:02 +0300

    create "create route for" restaurant pizzas

--- app/controllers/restaurant_pizzas_controller.rb
+++ app/controllers/restaurant_pizzas_controller.rb
@@ -1,7 +1,15 @@
 class RestaurantPizzasController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
-  def update
+  def create
+    res =
+      RestaurantPizza.create(
+        price: params[:price],
+        pizza_id: params[:pizza_id],
+        restaurant_id: params[:restaurant_id]
+      )
+
+    render json: res, status: :created
   end
 
   private
