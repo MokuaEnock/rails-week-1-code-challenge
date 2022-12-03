795b38fc19d71f9282b36a5b6b816621c4d4d067
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-12-03 06:53:33 +0300

    add response errors for pizza and restaurant pizza controller

--- app/controllers/restaurant_pizzas_controller.rb
+++ app/controllers/restaurant_pizzas_controller.rb
@@ -1,2 +1,12 @@
 class RestaurantPizzasController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+  def update
+  end
+
+  private
+
+  def render_not_found_response
+    render json: { error: "Author not found" }, status: :not_found
+  end
 end
