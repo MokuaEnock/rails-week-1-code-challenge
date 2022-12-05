1698e6d9e08f0220fe6044572a08e85fe519a8af
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-12-05 07:40:03 +0300

    add error boundaries for pizzas

--- app/controllers/pizzas_controller.rb
+++ app/controllers/pizzas_controller.rb
@@ -1,5 +1,7 @@
 class PizzasController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  rescue_from ActiveRecord::RecordInvalid,
+              with: :render_unprocessable_entity_response
 
   def index
     pizzas = Pizza.all
@@ -24,5 +26,12 @@ class PizzasController < ApplicationController
 
   def render_not_found_response
     render json: { error: "Pizza not found" }, status: :not_found
+  end
+
+  def render_unprocessable_entity_response(invalid)
+    render json: {
+             error: invalid.record.errors.full_messages
+           },
+           status: :unprocessable_entity
   end
 end
